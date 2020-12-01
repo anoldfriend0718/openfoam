1e93e52a137e19c7730ec6b73e224ae6bdea56ce
anoldfriend <xuqh12@tsinghua.org.cn>
2020-12-01 21:31:04 +0800

    test reactingDBSFoam by couetteFlow

---
+++ applications/test/reactingDBS/couetteFlow/run.sh
@@ -0,0 +1,10 @@
+foamCleanTutorials 
+logfile="run.log"
+if [ -f $logfile ]; then
+    rm $logfile
+fi
+
+touch run.log
+blockMesh | tee -a $logfile
+checkMesh | tee -a $logfile
+$FOAM_USER_APPBIN/reactingDBSFoam 2>&1 | tee -a $logfile
