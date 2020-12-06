5cf43032986a498c1ac09b60e76d255c5b34245f
anoldfriend <xuqh12@tsinghua.org.cn>
2020-12-06 20:57:18 +0800

    seperate read or create different fields into different Header files

--- applications/solvers/reactingDBSFoam/reactingDBSFoam.C
+++ applications/solvers/reactingDBSFoam/reactingDBSFoam.C
@@ -33,9 +33,7 @@ Description
 
 \*---------------------------------------------------------------------------*/
 
-#include "IOdictionary.H"
-#include "IOobject.H"
-#include "dimensionedScalarFwd.H"
+
 #include "fvCFD.H"
 #include "rhoThermo.H"
 #include "radiationModel.H"
@@ -53,30 +51,6 @@ int main(int argc, char *argv[])
     #include "createMesh.H"
     #include "createControl.H"
     #include "createFields.H"
-
-    Info<<"Reading the chemical properties"<<endl;
-    IOdictionary chemicalProperties
-    (
-        IOobject
-        (
-            "chemicalProperties",
-            runTime.constant(),
-            mesh,
-            IOobject::MUST_READ_IF_MODIFIED,
-            IOobject::NO_WRITE
-        )
-    );
-
-    dimensionedScalar gamma(chemicalProperties.lookup("gamma"));
-    dimensionedScalar reactionRate(chemicalProperties.lookup("reactionRate"));
-    dimensionedScalar Yeq(chemicalProperties.lookup("Yeq"));
-    dimensionedScalar reactionHeat(chemicalProperties.lookup("reactionHeat"));
-
-    Info<<"Stoichiometric Coefficient: "<<gamma<<endl;
-    Info<<"const solid dissolution rate: "<<reactionRate<<endl;
-    Info<<"equilibrium mass concentration: "<<Yeq<<endl;
-    Info<<"reaction heat: "<<reactionHeat<<endl;
-
     #include "createFieldRefs.H"
     #include "initContinuityErrs.H"
     #include "createTimeControls.H"
