6604e7771fa0ea24cad3324b47961b17c5e401c5
anoldfriend <xuqh12@tsinghua.org.cn>
2021-01-13 18:24:05 +0800

    create fields, correct reaction and solve coke evolution equation

--- applications/solvers/cokeCombustionFoam/cokeCombustionFoam.C
+++ applications/solvers/cokeCombustionFoam/cokeCombustionFoam.C
@@ -1,201 +1,198 @@
-#include "IOdictionary.H"
-#include "IFstream.H"
-#include "Time.H"
-#include "argList.H"
-#include "fvMesh.H"
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2012-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    cokeCombustionFoam
+
+Description
+    Solver for combustion with chemical reactions using a density based
+    thermodynamics package with enhanced buoyancy treatment.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
 #include "rhoReactionThermo.H"
-#include "thermo.H"
-#include "volFieldsFwd.H"
 #include "constSolidThermo.H"
 #include "cokeCombustion.H"
-#include "fvc.H"
+#include "pimpleControl.H"
+#include "fvOptions.H"
+#include "surfaceFieldsFwd.H"
+#include "volFieldsFwd.H"
 
-using namespace Foam;
 
-int main(int argc, char *argv[]) {
 
-    //#include "setRootCase.H"
-    argList args(argc, argv);
-    if (!args.checkRootCase()) {
-        Foam::FatalError.exit();
+int main(int argc, char *argv[])
+{
+    #include "postProcess.H"
+
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createControl.H"
+    #include "createFields.H"
+    #include "createFieldRefs.H"
+    #include "initContinuityErrs.H"
+    #include "createTimeControls.H"
+    #include "compressibleCourantNo.H"
+    #include "setInitialDeltaT.H"
+
+     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    volScalarField rhoCoke
+    (
+        IOobject
+        (
+            "solid",
+            runTime.timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimensionedScalar("rhoCoke", dimDensity, cokeThermo.density) 
+    );
+
+
+    while (runTime.run())
+    {
+        #include "readTimeControls.H"
+        #include "compressibleCourantNo.H"
+        #include "setDeltaT.H"
+
+        runTime++;
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        Info<< "solving reaction model"<<endl;
+        reaction.correct();
+
+        //solving coke volume evolution equation
+        fvScalarMatrix cokeEqn
+        (
+            fvm::ddt(rhoCoke,coke)
+          ==
+              reaction.Rs(coke)
+            + fvOptions(rhoCoke,coke)
+        );
+        cokeEqn.relax();
+        fvOptions.constrain(cokeEqn);
+
+        cokeEqn.solve();
+        fvOptions.correct(coke);
+
+        Info<<"updating the porous medium and related fields"<<endl;
+        eps=1-coke-rock;
+        rEps=1.0/(eps+SMALL);
+        rEpsf=fvc::interpolate(rEps);
+        phiByEpsf=phi*rEpsf;
+        rhoByEps=rho*rEps;
+
+        forAll(eps,celli)
+        {
+            if(eps[celli]>0.99)
+            {
+                solid[celli]=0.0;
+            }
+            else
+            {
+                solid[celli]=1.0;
+            }
+        }
+        volScalarField::Boundary& epsBf = eps.boundaryFieldRef();
+        volScalarField::Boundary& solidBf=solid.boundaryFieldRef();
+        forAll(epsBf,patchi)
+        {
+            forAll(epsBf[patchi],facei)
+            {
+                if(epsBf[patchi][facei]>0.99)
+                {
+                    solidBf[patchi][facei]=0.0;
+                }
+                else
+                {
+                    solidBf[patchi][facei]=1.0;
+                }
+            }
+        }
+
+        rK=rK0*(1.0-eps)*(1.0-eps)/max((eps*eps*eps),SMALL);
+        drag=fvc::average(mu*rK);
+        forAll(drag,celli)
+        {
+            if(solid[celli]<small) //==0
+            {
+                drag[celli]=0.0;
+            }
+        }
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+        // #include "cokeEqn.H"
+        // #include "rhoEqn.H"
+
+        // // --- Pressure-velocity PIMPLE corrector loop
+        // while (pimple.loop())
+        // { 
+        //     #include "UEqn.H"
+
+        //     #include "YEqn.H"
+
+        //     #include "EEqn.H"
+
+        //     // --- Pressure corrector loop
+        //     while (pimple.correct())
+        //     {
+        //         #include "pEqn.H"
+        //     }
+        // }
+
+        // rhof = thermo.rho();
+        
+        // #include "updateVariables.H"
+
+        // runTime.write();
+        
+        // Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+        //     << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+        //     << nl << endl;
     }
-    //#include "createTime.H"
-    Foam::Time runTime(Foam::Time::controlDictName, args);
-    //#include "createMesh.H"
-    fvMesh mesh(Foam::IOobject(fvMesh::defaultRegion, runTime.timeName(), runTime, IOobject::MUST_READ));
 
-    // Info << "Reading gas thermophysical properties\n" << endl;
-    // autoPtr<rhoReactionThermo> pThermo(rhoReactionThermo::New(mesh));
-    // rhoReactionThermo& thermo = pThermo();
-    // thermo.validate(args.executable(), "h", "e");
-    
-
-    // Info << "Reading solid thermophysical properties\n" << endl;
-    // IOdictionary thermoSolid
-    // (
-    //     IOobject
-    //     (
-    //         "thermo.Solid",
-    //         runTime.constant(),
-    //         mesh,
-    //         IOobject::MUST_READ_IF_MODIFIED,
-    //         IOobject::NO_WRITE
-    //     )
-    // );
-    // const constSolidThermo cokeThermo(thermoSolid.subDict("coke"));
-    // const constSolidThermo rockThermo(thermoSolid.subDict("rock"));
-    // Info<<"coke thermo: "<<cokeThermo<<endl;
-    // Info<<"rock thermo: "<<rockThermo<<endl;
-    
-    // Info<<"read U and create phi..."<<endl;
-    // volVectorField U
-    // (
-    //     IOobject
-    //     (
-    //         "U",
-    //         runTime.timeName(),
-    //         mesh,
-    //         IOobject::MUST_READ,
-    //         IOobject::AUTO_WRITE
-    //     ),
-    //     mesh
-    // );
-
-    // volScalarField rhof
-    // (
-    //     IOobject
-    //     (
-    //         "rho",
-    //         runTime.timeName(),
-    //         mesh,
-    //         IOobject::NO_READ,
-    //         IOobject::AUTO_WRITE
-    //     ),
-    //     thermo.rho()
-    // );
-
-    // surfaceScalarField phi
-    // (
-    //     IOobject
-    //     (
-    //         "phi",
-    //         runTime.timeName(),
-    //         mesh,
-    //         IOobject::READ_IF_PRESENT,
-    //         IOobject::AUTO_WRITE
-    //     ),
-    //     linearInterpolate(rhof*U) & mesh.Sf()
-    // );
-
-
-    //   //read eps, coke, rock fractions
-    // volScalarField eps
-    // (
-    //     IOobject
-    //     (
-    //         "eps",
-    //         runTime.timeName(),
-    //         mesh,
-    //         IOobject::MUST_READ,
-    //         IOobject::AUTO_WRITE
-    //     ),
-    //     mesh
-    // );
-    // volScalarField coke
-    // (
-    //     IOobject
-    //     (
-    //         "coke",
-    //         runTime.timeName(),
-    //         mesh,
-    //         IOobject::MUST_READ,
-    //         IOobject::AUTO_WRITE
-    //     ),
-    //     mesh
-    // );
-    // volScalarField rock
-    // (
-    //     IOobject
-    //     (
-    //         "rock",
-    //         runTime.timeName(),
-    //         mesh,
-    //         IOobject::NO_READ,
-    //         IOobject::NO_WRITE
-    //     ),
-    //     1-eps-coke
-    // );
-    // Info<<"fluid porosity: "<<coke.field()<<", solid fraction: "<<(1-eps)->field()<<endl;
-    // Info<<"coke fraction: "<<coke.field()<<", rock fraction: "<<rock.field()<<endl;
-
-    // cokeCombustion combustion(mesh,thermo,cokeThermo,rockThermo);
-
-    // for(label i=0;i<2;i++)
-    // {
-    //     Info<<endl<<"new step..."<<endl;
-    //     combustion.correct();
-
-    //     Info<<"Test the RR..."<<endl;
-    //     Info<<"Time-splitting RR O2: "<<combustion.RRO2().field()<<endl;
-    //     Info<<"Time-splitting RR CO2: "<<combustion.RRCO2().field()<<endl;
-    //     Info<<"Time-splitting RR coke: "<<combustion.RRCoke().field()<<endl;
-    //     Info<<"Normal RR O2: "<<combustion.calculateTransientRRO2()().field()<<endl;
-
-    //     Info<<"Test the su matrix..."<<endl;
-    //     volScalarField& YO2=thermo.composition().Y(0);
-    //     volScalarField& YCO2=thermo.composition().Y(2);
-    //     Info<<"R O2: "<<(combustion.R(YO2)&YO2)->field()<<endl;
-    //     Info<<"R CO2: "<<(combustion.R(YCO2)&YCO2)->field()<<endl;
-    //     Info<<"R coke: "<<(combustion.Rs(coke)&coke)->field()<<endl;
-
-    //     Info<<"Test Qdot..."<<endl;
-    //     Info<<"Qdot: "<<combustion.Qdot()->field()<<endl;
-
-    //     runTime++;
-
-    //     Info<<"simulate solving solid equation by update the eps and coke..."<<endl;
-    //     eps+=0.1;
-    //     coke-=0.1;
-
-    //     Info<<"simulate solving energy equation by update the enthalpy..."<<endl;
-    //     // Info<<"Cp: "<<thermo.Cp()<<endl;
-    //     volScalarField& he=thermo.he();
-    //     scalar he1=1200000;
-    //     forAll(he, celli)
-    //     {
-    //         he[celli]=he1;
-    //     }
-
-    //     volScalarField::Boundary& heBf = he.boundaryFieldRef();
-    //     forAll(heBf, patchi)
-    //     {
-    //         scalarField& hep=heBf[patchi];
-    //         forAll(hep,facei)
-    //         {
-    //             hep[facei]=he1;
-    //         }
-    //     }
-
-    
-    //     thermo.correct();
-
-        
-    //    Info<<"simulate solving pressure equation by update the pressure..."<<endl;
-    //    volScalarField& p=thermo.p();
-    //    p*=1.05;
-
-    //    Info<<"simulate solving the species equations by updating Y..."<<endl;
-    //    YO2-=0.03;
-    //    YCO2 +=0.03;
-
-
-
-        
-
-        
-    // }
-
-
+    Info<< "End\n" << endl;
 
     return 0;
 }
