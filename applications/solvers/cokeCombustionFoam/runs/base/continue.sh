2acbdf28d7a7c6d7e422634fc85e4d7f04192bec
anoldfriend <xuqh12@tsinghua.org.cn>
2021-01-23 12:30:15 +0800

    restructure the cokeCombustionFoam foalder

--- applications/solvers/cokeCombustionFoam/runs/base/continue.sh
+++ applications/solvers/cokeCombustionFoam/runs/base/continue.sh
@@ -1,5 +1,5 @@
 # decomposePar | tee -a run.log 
-mpirun -np 4 $FOAM_USER_APPBIN/dissolutionDBSFoam -parallel 2>&1 >> run.log 
+mpirun -np 4 $FOAM_USER_APPBIN/cokeCombustionFoam -parallel 2>&1 >> run.log 
 reconstructPar | tee -a run.log 
 
 # $FOAM_USER_APPBIN/dissolutionDBSFoam 2>&1 | tee -a run.log 
--- applications/solvers/cokeCombustionFoam/runs/base/run.sh
+++ applications/solvers/cokeCombustionFoam/runs/base/run.sh
@@ -1,2 +1,5 @@
+#
+cd  ../../
 wmake
+cd -
 $FOAM_USER_APPBIN/cokeCombustionFoam 2>&1 > run.log
