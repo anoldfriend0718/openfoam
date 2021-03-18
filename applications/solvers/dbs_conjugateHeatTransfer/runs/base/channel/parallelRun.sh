441c2aaf967cc03adafa9cc07168733dea90c908
anoldfriend <xuqh12@tsinghua.org.cn>
2021-03-18 17:54:35 +0800

    solve the mesh error in the conjugate heat transfer case

--- applications/solvers/dbs_conjugateHeatTransfer/runs/base/channel/parallelRun.sh
+++
@@ -1,9 +0,0 @@
-foamCleanTutorials | tee run.log 
-blockMesh  | tee -a run.log 
-checkMesh  | tee  -a run.log 
-
-decomposePar | tee -a run.log 
-mpirun -np 8 $FOAM_USER_APPBIN/dbsConjugateHeatTransfer -parallel 2>&1 >> run.log 
-reconstructPar #| tee -a run.log 
-
-# $FOAM_USER_APPBIN/dissolutionDBSFoam 2>&1 | tee -a run.log 
---
+++ applications/solvers/dbs_conjugateHeatTransfer/runs/base/fluidflowByIcoDBSFoam/run.sh
@@ -0,0 +1,9 @@
+foamCleanTutorials | tee run.log 
+blockMesh  | tee -a run.log 
+checkMesh  | tee  -a run.log 
+
+# $FOAM_USER_APPBIN/icoDBSFoam  2>&1 | tee  -a run.log 
+
+decomposePar | tee -a run.log 
+mpirun -np 8 $FOAM_USER_APPBIN/icoDBSFoam -parallel 2>&1 | tee -a run.log 
+reconstructPar | tee -a run.log 
