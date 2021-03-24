8ddd918f6d2ef11c24f3146c248dfc94b7942240
anoldfriend <xuqh12@tsinghua.org.cn>
2021-03-24 16:14:48 +0800

    validate the specific surface area

---
+++ applications/solvers/dbs_reactiveThermalFoam/runs/specificSurfaceArea/Test-ReactiveThermalCaseSpecificSurfaceArea.C
@@ -0,0 +1,60 @@
+#include "fvCFD.H"
+#include "volFieldsFwd.H"
+
+using namespace Foam;
+int main(int argc, char *argv[]) 
+{
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    volScalarField eps
+    (
+        IOobject
+        (
+            "eps",
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh
+    );
+
+    volScalarField epsGrad //without filter depth
+    (
+        "epsGrad",
+        mag(fvc::grad(eps))
+    );
+
+    epsGrad.write();
+
+    volScalarField speciesSurfaceArea
+    {
+        "speciesSurfaceArea",
+        2*mag(fvc::grad(eps))*(1-eps*eps)
+    };
+
+    volScalarField surfaceArea
+    {
+        "surfaceArea",
+        speciesSurfaceArea*Foam::pow(1e-6,1)
+    }; 
+
+    surfaceArea.write();
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+}
