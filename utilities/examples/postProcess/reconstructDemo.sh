942cf7bd8ba618eb95bbb35e823e1ab02716ab28
anoldfriend <xuqh12@tsinghua.org.cn>
2021-04-29 23:47:40 +0800

    add requirement.txt for the utilities package

--- utilities/examples/postProcess/reconstructDemo.sh
+++ utilities/examples/postProcess/reconstructDemo.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
+source $WM_PROJECT_USER_DIR/.venv/bin/activate
 caseDir=$WM_PROJECT_USER_DIR/applications/solvers/cokeCombustionFoam/SegregatedSteps/runs/complicatedPorousMedia/combustions/tiny/convectionLimited
 pyscipt=$WM_PROJECT_USER_DIR/utilities/postProcess/pyResconstruct.py
 fieldNames='["U","T","p","rho","O2","CO2","eps","coke","cokeRectionRate","Qdot"]'
-timeNames=all
+timeNames=['0.1']
 sampleRate=1
 workerNum=30
 overWrite=false
 dataFolder=postProcess
 
 if $overWrite; then
-    python  $pyscipt -c $caseDir -t $timeNames -f $fieldNames -n $workerNum -r $sampleRate -s $dataFolder -w
+    python3  $pyscipt -c $caseDir -t $timeNames -f $fieldNames -n $workerNum -r $sampleRate -s $dataFolder -w
 else
-    python  $pyscipt -c $caseDir -t $timeNames -f $fieldNames -n $workerNum -r $sampleRate -s $dataFolder 
+    python3  $pyscipt -c $caseDir -t $timeNames -f $fieldNames -n $workerNum -r $sampleRate -s $dataFolder 
 fi
