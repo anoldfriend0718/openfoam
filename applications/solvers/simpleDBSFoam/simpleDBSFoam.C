ad9686792f2de334799080f03eaa0c1fd14c0205
anoldfriend <xuqh12@tsinghua.org.cn>
2021-05-12 23:28:46 +0800

    start to develop the simple dbs foam

---
+++ applications/solvers/simpleDBSFoam/simpleDBSFoam.C
@@ -0,0 +1,161 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    icoGravityFoam
+
+Description
+    Transient solver for incompressible, laminar flow of Newtonian fluids with 
+    the user-specified gravity acceleration
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "fvmSup.H"
+#include "pisoControl.H"
+#include "surfaceFieldsFwd.H"
+#include "surfaceInterpolate.H"
+#include "volFieldsFwd.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    pisoControl piso(mesh);
+
+    #include "createFields.H"
+    #include "initContinuityErrs.H"
+    #include "createTimeControls.H"
+    #include "CourantNo.H"
+    #include "setInitialDeltaT.H"
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+    
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.loop())
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+        
+        #include "readTimeControls.H"
+        #include "CourantNo.H"
+        #include "setDeltaT.H"
+
+        // Momentum predictor
+
+        fvVectorMatrix UEqn
+        (
+            (1./eps)*fvm::ddt(U)
+          + (1./eps)*fvm::div(phiByEpsf, U)
+          - (1./eps)*fvm::laplacian(nu, U)
+          + fvm::SuSp(drag,U)
+        );
+
+        if (piso.momentumPredictor())
+        {
+            solve(UEqn == (-fvc::grad(p) + g ));
+        }
+
+        // --- PISO loop
+        while (piso.correct())
+        {
+            volScalarField rAU(1.0/UEqn.A());
+            //add the phi caused by gravity acceleration 
+            surfaceScalarField rAUf("rAUf", fvc::interpolate(rAU));
+            volVectorField HbyA(constrainHbyA(rAU*UEqn.H(), U, p));
+            surfaceScalarField phig(rAUf*(g & mesh.Sf()));
+
+            surfaceScalarField phiHbyA
+            (
+                "phiHbyA",
+                fvc::flux(HbyA)
+                // + fvc::interpolate(rAU)*fvc::ddtCorr(U, phi)
+                + fvc::interpolate(rAU)*fvc::ddtCorr(rEps,U, phiByEpsf)
+            );
+
+            adjustPhi(phiHbyA, U, p);
+
+            phiHbyA+=phig;
+
+            // Update the pressure BCs to ensure flux consistency
+            constrainPressure(p, U, phiHbyA, rAU);
+
+            // Non-orthogonal pressure corrector loop
+            while (piso.correctNonOrthogonal())
+            {
+                // Pressure corrector
+
+                fvScalarMatrix pEqn
+                (
+                    fvm::laplacian(rAU, p) == fvc::div(phiHbyA)
+                );
+
+                pEqn.setReference(pRefCell, pRefValue);
+
+                pEqn.solve();
+
+                if (piso.finalNonOrthogonalIter())
+                {
+                    phi = phiHbyA - pEqn.flux();
+                }
+            }
+
+            #include "continuityErrs.H"
+
+
+            U = HbyA - rAU*fvc::grad(p)+rAU*g;
+            U.correctBoundaryConditions();
+
+            phiByEpsf = phi*repsf;
+        }
+
+        runTime.write();
+
+        tmp<volVectorField> tUResidual(U-U.oldTime());
+        const volVectorField& UResidual=tUResidual.ref();
+        const Foam::Vector<scalar> normUResidual=gSumCmptProd(UResidual,UResidual);
+        const Foam::Vector<scalar> normU=gSumCmptProd(U,U);
+        Foam::Vector<scalar> relativeUResidual;
+        for (direction cmpt=0; cmpt < normU.size(); cmpt++)
+        {
+            relativeUResidual[cmpt] = Foam::sqrt(normUResidual[cmpt]/(normU[cmpt]+SMALL));
+        }
+        Info<<"relative error: Ux: "<<relativeUResidual[0]<<" Uy: "<<relativeUResidual[1]<<endl;
+
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
